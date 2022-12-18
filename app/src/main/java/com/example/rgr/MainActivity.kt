ca245c5bb107c660aff80f552f907eef5dfffad1
Dima Muzychenko <robin700521@gmail.com>
2022-12-18 04:17:42 +0200

    RGR with some able to change photoes

--- app/src/main/java/com/example/rgr/MainActivity.kt
+++ app/src/main/java/com/example/rgr/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.rgr
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
 import com.example.rgr.databinding.ActivityMainBinding
 import jp.wasabeef.glide.transformations.BlurTransformation
-import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -30,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun onGetRandomImage():Boolean {
         var key = binding.editText.text.toString()
         if(key.isBlank()){
@@ -37,20 +37,18 @@ class MainActivity : AppCompatActivity() {
             return true
         }
 
+        val multi = MultiTransformation<Bitmap>(
+            BlurTransformation(binding.seekBarBlur.progress.toInt())//наскільки сильно заблюрювати
+        )
+
         var enkodedAssociatingWord = URLEncoder.encode(key, StandardCharsets.UTF_8.name())
         Glide.with(this)
             .load("https://source.unsplash.com/random/800x600?$enkodedAssociatingWord")
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)//щоб не видавалася постійно 1 й та сама фотка
+            .apply (RequestOptions.bitmapTransform(multi))
             .into(binding.testImage)
         return false
     }
 
-    //        val multi = MultiTransformation<Bitmap>(
-//            BlurTransformation(25),
-//            RoundedCornersTransformation(128, 0, RoundedCornersTransformation.CornerType.BOTTOM)
-//        )
-//        Glide.with(this).load("https://source.unsplash.com/random/800x600")
-//            .apply(RequestOptions.bitmapTransform(multi))
-//            .into(binding.testImage)
 }
